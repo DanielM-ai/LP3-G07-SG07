417ed4e0b68fcca622fd9afe6dd3bac486077fa1
DanielM-ai <daniel.mansilla@estudiante.ucsm.edu.pe>
2025-10-02 07:57:10 -0500

    444444

--- Ejercicio4/Persona.java
+++ Ejercicio4/Persona.java
@@ -1,57 +1,29 @@
-package LP3;
+package ejercicio_04;
 
 public class Persona {
-    private int id;
     private String nombre;
-    private String apellido;
-    private Cuenta cuenta; // Atributo que representa la parte de la composición
+    private int edad;
 
-    // Constructor de Persona
-    public Persona(int id, String nombre, String apellido) {
-        this.id = id;
+    public Persona(String nombre, int edad) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        // Se crea la instancia de Cuenta dentro de Persona, estableciendo la composición.
-        // El número de cuenta se genera a partir del ID de la persona.
-        this.cuenta = new Cuenta(id * 100);
-    }
-
-    // Métodos Getters y Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Cuenta getCuenta() {
-        return cuenta;
-    }
-
-    public void setCuenta(Cuenta cuenta) {
-        this.cuenta = cuenta;
+        this.edad = edad;
     }
 
     @Override
     public String toString() {
-        // Retorna la información completa de la persona, incluyendo los datos de su cuenta
-        return "Persona [ID=" + id + ", Nombre=" + nombre + ", Apellido=" + apellido + "]\n  -> " + cuenta.toString();
+        return "Persona{" + nombre + ", " + edad + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Persona persona = (Persona) obj;
+        return edad == persona.edad && nombre.equals(persona.nombre);
+    }
+    
+    @Override
+    public int hashCode() {
+        return nombre.hashCode() + edad; 
     }
 }

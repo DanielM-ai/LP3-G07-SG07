1cbb06752d9deaa37d4dbbc02a70ef49a3b325db
DanielM-ai <daniel.mansilla@estudiante.ucsm.edu.pe>
2025-10-02 08:00:52 -0500

    444444

---
+++ Fase 2/Lab01/Ejercicios/ejercicio_04/Persona.java
@@ -0,0 +1,29 @@
+package ejercicio_04;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + nombre + ", " + edad + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Persona persona = (Persona) obj;
+        return edad == persona.edad && nombre.equals(persona.nombre);
+    }
+    
+    @Override
+    public int hashCode() {
+        return nombre.hashCode() + edad; 
+    }
+}
